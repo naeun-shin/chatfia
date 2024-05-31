f6d6114e55a28ed5ea485fa0decfc982c3fe6e0a
torong-dev <hi_ryuji@naver.com>
2024-05-31 06:50:25 +0000

    chore: rename .env to .env.local

---
+++ src/components/rule/RoleIntroduction.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RoleIntroduction: React.FC = () => {
+  return (
+    <div>
+      <div className="flex items-end justify-center mt-40 pt-20 pb-20 space-x-2 bg-custom">
+        <p className="text-4xl font-bold">직업 소개</p>
+        <p className="font-bold">Role introduction</p>
+      </div>
+    </div>
+  );
+};
+
+export default RoleIntroduction;
--- src/pages/rule.tsx
+++ src/pages/rule.tsx
@@ -1,12 +1,14 @@
-import Header from "@/components/header/Header";
 import React from "react";
+import Header from "@/components/header/Header";
 import GameRules from "../components/rule/GameRules";
+import RoleIntroduction from "@/components/rule/RoleIntroduction";
 
 export default function Rule() {
   return (
     <div>
       <Header />
       <GameRules />
+      <RoleIntroduction />
     </div>
   );
 }
