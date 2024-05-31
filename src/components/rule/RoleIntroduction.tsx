5fee18074f71aa11794efd87e580309001e9382e
torong-dev <hi_ryuji@naver.com>
2024-05-31 21:27:23 +0000

    chore: 브랜치 전환을 위한 임시 커밋

--- src/components/rule/RoleIntroduction.tsx
+++ src/components/rule/RoleIntroduction.tsx
@@ -5,7 +5,7 @@ const RoleIntroduction: React.FC = () => {
   return (
     <>
       <div className="bg-custom">
-        <div className="flex items-end justify-center mt-40 pt-20 space-x-2">
+        <div className="mt-40 flex items-end justify-center space-x-2 pt-20">
           <p className="text-4xl font-bold">직업 소개</p>
           <p className="font-bold">Role introduction</p>
         </div>
