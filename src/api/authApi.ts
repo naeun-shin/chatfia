804d4baa3235516eada77beace188c0f43d1141c
torong-dev <hi_ryuji@naver.com>
2024-06-02 01:21:35 +0000

    fix: API 연결을 위한 baseURL 업데이트 및 변경된 API 명세서 적용

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -9,15 +9,15 @@ export const api = {
   /* 회원가입 */
   signup: async (signUpUserData: SignUpType): Promise<SignUpResponse> => {
     const response = await instance.post<SignUpResponse>(
-      "/api/auth/signup",
-      signUpUserData
+      "/auth/signup",
+      signUpUserData,
     );
     return response.data;
   },
 
   /* 로그인 */
   login: async (loginUserData: LoginType) => {
-    const response = await instance.post("/api/auth/login", loginUserData);
+    const response = await instance.post("/auth/login", loginUserData);
     return response.data;
   },
 };
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // token이 없는 경우의 instance => 로그인 및 회원가입 API 연결 시도 때 사용하는 instance
 export const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  // baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://api.chatfia.online",
   headers: {
     "Content-Type": "application/json",
   },
@@ -24,14 +25,14 @@ instance.interceptors.response.use(
           "불러오기에 에러 발생했습니다 : " +
             error.response.data.error +
             " : " +
-            error.response.data.status
+            error.response.data.status,
         );
       } else {
         alert(error.response.data.message);
       }
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 // 헤더에 토큰을 담는 instance
