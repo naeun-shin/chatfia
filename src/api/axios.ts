da390a212d38ec569a357120a156fdc496f4ff08
torong-dev <hi_ryuji@naver.com>
2024-06-07 07:56:57 +0000

    feat: 토큰이 있는 경우의 axios 인스턴스 추가 및 응답과 요청 인터셉터 설정 - 토큰 관리를 위한 useCookie와 useStorage 훅 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import { setCookie } from "@/hooks/useCookie";
+import { getStorage, setStorage } from "@/hooks/useStorage";
 
-// token이 없는 경우의 instance => 로그인 및 회원가입 API 연결 시도 때 사용하는 instance
+// 토큰이 없는 경우의 instance => 로그인 및 회원가입 API 연결 시도 때 사용하는 instance
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
@@ -11,6 +13,14 @@ export const instance = axios.create({
 // 응답 인터셉터
 instance.interceptors.response.use(
   (response) => {
+    const accessToken = response.headers.accesstoken;
+    const refreshToken = response.headers.refreshtoken;
+
+    if (accessToken && refreshToken) {
+      setStorage("accessToken", accessToken);
+      setCookie("refreshToken", refreshToken);
+    }
+
     return response;
   },
   (error) => {
@@ -34,4 +44,24 @@ instance.interceptors.response.use(
   },
 );
 
-// 헤더에 토큰을 담는 instance
+// 토큰이 있는 경우의 instance => 로그인 후에 사용하는 instance
+export const instanceWithToken = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 요청 인터셉터를 사용하여 Authorization 헤더 설정
+instanceWithToken.interceptors.request.use(
+  (config) => {
+    const token = getStorage("accessToken");
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
